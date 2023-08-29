c73f42328c340239538bcf681deda1a04be2b140
Ed-cred <edwardandrew63@gmail.com>
2023-08-29 13:07:34 +0300

    Added JSON handler

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	yamlFile := flag.String("yaml", "", "YAML file to read URLs from formatted as -path:... url:...")
+	jsonFile := flag.String("json", "", "JSON file to read URLs from")
 	flag.Parse()
 	mux := defaultMux()
 	// Build the MapHandler using the mux as the fallback
@@ -23,29 +24,36 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	var yamlBytes []byte
 	if *yamlFile != "" {
-		b, err := os.ReadFile(*yamlFile)
+		yamlBytes, err := os.ReadFile(*yamlFile)
 		if err != nil {
 			log.Fatalf("Failed to read provided yaml file: %v", err)
 		}
-		yamlBytes = b
-		
-	} else {
-		yamlBytes = []byte(`
-		- path: /urlshort
-		url: https://github.com/gophercises/urlshort
-		- path: /urlshort-final
-		url: https://github.com/gophercises/urlshort/tree/solution
-		`)
-	}
-	yamlHandler, err := urlshort.YAMLHandler(yamlBytes, mapHandler)
+		yamlHandler, err := urlshort.YAMLHandler(yamlBytes, mapHandler)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println("Starting the server on :8080")
+		http.ListenAndServe(":8080", yamlHandler)
 
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	} else if *jsonFile != "" {
+
+		jsonBytes, err := os.ReadFile(*jsonFile)
+		if err != nil {
+			log.Fatalf("Failed to read provided yaml file: %v", err)
+		}
+
+		jsonHandler, err := urlshort.JSONHandler(jsonBytes, mapHandler)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Starting the server on :8080")
+		http.ListenAndServe(":8080", jsonHandler)
+	} else {
+
+		fmt.Println("Starting the server on :8080")
+		http.ListenAndServe(":8080", mapHandler)
+	}
 }
 
 func defaultMux() *http.ServeMux {
